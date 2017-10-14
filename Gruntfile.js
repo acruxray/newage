d9ff8eca2beb1f8b34c87f488d46fb9f08bfaac3
Nazar Mozol <xfce4@yandex.ua>
2017-10-15 02:39:36 +0300

    Adding Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+          main: {
+            src: 'src/**/*.css',  // All css files in src dir
+            dest: 'dist/css/newage.css'
+          }
+        },
+
+        cssmin: {
+          main: {
+            src: 'dist/css/newage.css',
+            dest: 'dist/css/newage.min.css'
+          }
+        },
+
+        // version headers for files
+        usebanner: {
+            taskName: {
+                options: {
+                    position: 'top',
+                    banner: '/*\n' +
+                            ' * Newage v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+                            ' * Copyright <%= grunt.template.today("yyyy") %> Newage\n' +
+                            ' * <%= pkg.license %>\n' +
+                            ' */',
+                    linebreak: true
+                },
+                files: {
+                    src: [
+                        'dist/css/newage.css',
+                        'dist/css/newage.min.css'
+                    ]
+                }
+            }
+        },
+
+        copy: {
+          main: {
+            src: 'dist/css/newage.min.css',
+            dest: 'docs/css/newage.min.css'
+          }
+        },
+
+        // Test server. For use run [grunt connect]
+        connect: {
+    	  server: {
+    	    options: {
+    	      port: 8000,
+    	      base: 'docs',
+              keepalive: true
+    	    }
+    	  }
+    	}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-banner');
+
+    grunt.registerTask('default', ['concat', 'cssmin', 'usebanner', 'copy']);
+};
