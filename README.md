4b6cb47d544f09b9ac8bbd96f4336634c0122d76
Nazar Mozol <xfce4@yandex.ua>
2017-12-18 13:08:17 +0200

    sidebar-header and helpers: link disabled

--- README.md
+++ README.md
@@ -4,18 +4,18 @@ A minimalistic CSS framework in the style of material design.
 
 ## Versions
 
-* Current: v0.4.4
-* Latest stable: v0.4.2
+* Current: v0.4.6
+* Latest stable: v0.4.6
 
 ## Install
 
 Set to `head` block of html-page:
 
 ```html
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/acruxray/newage@0.4.2/dist/css/newage.min.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/acruxray/newage@0.4.6/dist/css/newage.min.css">
 ```
 And, if you intend to use the *sidebar*, add to `body` block before `</body>` tag:
 
 ```html
-<script src="https://cdn.jsdelivr.net/gh/acruxray/newage@0.4.2/dist/js/newage.min.js"></script>
+<script src="https://cdn.jsdelivr.net/gh/acruxray/newage@0.4.6/dist/js/newage.min.js"></script>
 ```
--- docs/js/main.js
+++ docs/js/main.js
@@ -2,14 +2,17 @@
 var app = new Vue({
   el: '#app',
   data: {
-  	message: 'loading ...'
+  	message: 'loading ...',
+    latestCommitHash: '',
+    latestCommitUrl: '',
+    latestCommitDate: ''
   },
   computed: {
 	dao() {
 		var z = window.location.hash,
 			a = (z == '') ? 'index' : z.split('/')[1];
 
-		g(a, function(r) {
+		g('base/' + a + '.html', function(r) {
 			app.message = r;
             hL();
 		});
@@ -17,7 +20,7 @@ var app = new Vue({
   },
   methods: {
 	show: function(a) {
-		g(a, function(r) {
+		g('base/' + a + '.html', function(r) {
 			app.message = r;
             hL();
 		});
@@ -31,7 +34,7 @@ app.dao;
 
 
 function g(a, cb) {
-	axios.get('base/' + a + '.html')
+	axios.get(a)
 		.then(function(response) {
 			cb(response.data);
 		}).catch(function (error) {
@@ -39,6 +42,17 @@ function g(a, cb) {
 		});
 }
 
+
 function hL() {
     setTimeout(function() { Prism.highlightAll() }, 300);
+}
+
+
+window.onload = function() {
+    g('https://api.github.com/repos/acruxray/newage/commits', function(r) {
+        console.log(r[0].sha)
+        app.latestCommitHash = r[0].sha;
+        app.latestCommitUrl  = r[0].html_url;
+        app.latestCommitDate = moment(r[0].commit.committer.date).fromNow();
+    });
 }
