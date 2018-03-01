2f78a47a49f4689c36eb6884ab1a212831b0d432
Nazar Mozol <xfce4@yandex.ua>
2018-03-01 18:40:30 +0200

    Adding handle switch mode for sidebar ans fix long tibles display logic

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -4,7 +4,23 @@ function sideBar(o) {
         sidebarBtn    = document.getElementById(o.collapseButton),
         contentLocker = document.getElementById(o.contentLocker),
         sublist       = 'sidebar-sublist',
-        a             = 'active';
+        a             = 'active',
+        pro           = o.pro ? o.pro : false,
+        state         = o.state ? o.state : false;
+
+    if (pro) {
+        sidebarBtn.classList.add('sidebar-btn-view');
+        if (!state) {
+            var sC = false; // page onload
+            toogleClassPro();
+        } else {
+            sidebar.classList.add('sidebar-dyn');
+            var el = document.getElementsByClassName('sidebar-push');
+            for (var i = 0, l = el.length; i < l; i++) {
+                el[i].classList.add('sidebar-push-dyn');
+            }
+        }
+    }
 
     sidebarBtn.onclick = function (e) {
         e.preventDefault();
@@ -17,8 +33,8 @@ function sideBar(o) {
         }
     };
 
+
     sidebar.onclick = function(e) {
-        // console.log(e.target.parentNode.nextElementSibling.className)
         // collapse/expand sublist by click to parent item
         if ( // if [a] contains another elements
             e.target.parentNode.tagName === 'A' &&
@@ -44,9 +60,13 @@ function sideBar(o) {
     };
 
     function toggle() {
-        toggleClass(contentLocker);
-        toggleClass(sidebar);
-        toggleClass(sidebarBtn);
+        if (pro && document.body.clientWidth > 600) {
+            toogleClassPro();
+        } else {
+            toggleClass(contentLocker);
+            toggleClass(sidebar);
+            toggleClass(sidebarBtn);
+        }
     }
 
     function toggleClass(el) {
@@ -55,6 +75,33 @@ function sideBar(o) {
         } else {
             el.classList.remove(a);
         }
+    }
+
+    function toogleClassPro() {
+        if (sidebar.className.split(' ').indexOf('sidebar-hidden') < 0) {
+            sidebar.classList.add('sidebar-hidden');
+            if (sC) {
+                sidebar.classList.add('sidebar-dyn');
+            }
+
+            var el = document.getElementsByClassName('sidebar-push');
+            for (var i = 0, l = el.length; i < l; i++) {
+                el[i].classList.add('sidebar-push-hidden');
+                if (sC) {
+                    el[i].classList.add('sidebar-push-dyn');
+                }
+            }
+        } else {
+            sidebar.classList.remove('sidebar-hidden');
+            sidebar.classList.add('sidebar-dyn');
+            var el = document.getElementsByClassName('sidebar-push');
+            for (var i = 0, l = el.length; i < l; i++) {
+                el[i].classList.remove('sidebar-push-hidden');
+                el[i].classList.add('sidebar-push-dyn');
+            }
+        }
+
+        sC = true;
     }
 
 }
